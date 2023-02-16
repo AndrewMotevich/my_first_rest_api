b870d8d6b6a9c29580acbc522e4e635784badca7
Andrew <101500007+AndrewMotevich@users.noreply.github.com>
2023-02-16 22:07:31 +0300

    fix: update

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 /** @format */
 
-var { MongoClient } = require("mongodb");
+import { MongoClient } from "mongodb";
 // const uri = 'mongodb://localhost:27017';
 
 // const cloudURI = 'mongodb+srv://andrewmotevich:a9gwZbPpNbuICb29@cluster0.b23op1h.mongodb.net/?retryWrites=true&w=majority'
 const cloudURI =
   "mongodb+srv://vercel-admin-user:MCm8xsb6HBmZkcGP@cluster0.b23op1h.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 const client = new MongoClient(cloudURI);
-const express = require("express");
+import express from"express";
 const app = express();
 
 app.use(express.json());
