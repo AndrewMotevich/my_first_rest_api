f4e92727eeb0438df6146d244ee0be9c1e3776b8
Andrew <101500007+AndrewMotevich@users.noreply.github.com>
2023-02-16 21:30:01 +0300

    fix: update

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 /** @format */
 
-var MongoClient = require('mongodb');
+var {MongoClient} = require("mongodb");
 // const uri = 'mongodb://localhost:27017';
 // const cloudURI = 'mongodb+srv://andrewmotevich:a9gwZbPpNbuICb29@cluster0.b23op1h.mongodb.net/?retryWrites=true&w=majority'
-const cloudURI = 'mongodb+srv://vercel-admin-user:MCm8xsb6HBmZkcGP@cluster0.b23op1h.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+const cloudURI =
+  "mongodb+srv://vercel-admin-user:MCm8xsb6HBmZkcGP@cluster0.b23op1h.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 const client = new MongoClient(cloudURI);
 
 var express = require("express");
@@ -12,79 +13,103 @@ var app = express();
 app.use(express.json());
 
 app.get("/listUsers", async function (req, res) {
-  const usersArray = await client.db('myDatabase').collection('users').find().toArray();
+  const usersArray = await client
+    .db("myDatabase")
+    .collection("users")
+    .find()
+    .toArray();
   res.end(JSON.stringify(usersArray));
 });
 
 app.post("/addUser", async function (req, res) {
   // First read existing users.
   const reqData = req.body;
-  if (Object.keys(reqData).length === 4 && Object.keys(reqData).includes('userName') && Object.keys(reqData).includes('userPassword') && Object.keys(reqData).includes('email') && Object.keys(reqData).includes('phone')){
-    const newUser = await client.db('myDatabase').collection('users').insertOne(reqData);
-    const checkUser = await client.db('myDatabase').collection('users').findOne({"userName": `${reqData.userName}`})
+  if (
+    Object.keys(reqData).length === 4 &&
+    Object.keys(reqData).includes("userName") &&
+    Object.keys(reqData).includes("userPassword") &&
+    Object.keys(reqData).includes("email") &&
+    Object.keys(reqData).includes("phone")
+  ) {
+    const newUser = await client
+      .db("myDatabase")
+      .collection("users")
+      .insertOne(reqData);
+    const checkUser = await client
+      .db("myDatabase")
+      .collection("users")
+      .findOne({ userName: `${reqData.userName}` });
     res.end(JSON.stringify(checkUser));
-  }
-  else {
-    res.status(500)
-    res.end('Some Error')
+  } else {
+    res.status(500);
+    res.end("Some Error");
   }
 });
 
 app.get("/:name", async function (req, res) {
   // First read existing users.
   try {
-    const user = await client.db('myDatabase').collection('users').findOne({"userName": `${req.params.name}`});
-    if (user === null){
+    const user = await client
+      .db("myDatabase")
+      .collection("users")
+      .findOne({ userName: `${req.params.name}` });
+    if (user === null) {
       res.status(404);
-      res.end("This user do not exist")
+      res.end("This user do not exist");
+    } else {
+      res.end(JSON.stringify(user));
     }
-    else{
-      res.end(JSON.stringify(user))
-    }
-  }
-  catch (err) {
-    res.status(500)
+  } catch (err) {
+    res.status(500);
     res.end(err);
   }
 });
 
-app.delete('/deleteUser/:name', async function (req, res) {
+app.delete("/deleteUser/:name", async function (req, res) {
   // First read existing users.
   try {
-    const user = await client.db('myDatabase').collection('users').findOne({"userName": `${req.params.name}`});
-    if (user === null){
+    const user = await client
+      .db("myDatabase")
+      .collection("users")
+      .findOne({ userName: `${req.params.name}` });
+    if (user === null) {
       res.status(404);
-      res.end("This user do not exist")
+      res.end("This user do not exist");
+    } else {
+      await client
+        .db("myDatabase")
+        .collection("users")
+        .deleteOne({ userName: `${req.params.name}` });
+      res.end("This user deleted");
     }
-    else{
-      await client.db('myDatabase').collection('users').deleteOne({"userName": `${req.params.name}`});
-      res.end("This user deleted")
-    }
-  }
-  catch (err) {
-    res.status(500)
+  } catch (err) {
+    res.status(500);
     res.end(err);
   }
-})
+});
 
-app.patch('/updateUser/:name', async function (req, res){
+app.patch("/updateUser/:name", async function (req, res) {
   const reqData = req.body;
   try {
-    const user = await client.db('myDatabase').collection('users').findOne({"userName": `${req.params.name}`});
-    if (user === null){
+    const user = await client
+      .db("myDatabase")
+      .collection("users")
+      .findOne({ userName: `${req.params.name}` });
+    if (user === null) {
       res.status(404);
-      res.end("This user do not exist")
+      res.end("This user do not exist");
+    } else {
+      await client
+        .db("myDatabase")
+        .collection("users")
+        .updateOne({ userName: `${req.params.name}` }, { $set: reqData });
+      res.end(JSON.stringify(reqData));
     }
-    else{
-      await client.db('myDatabase').collection('users').updateOne({"userName": `${req.params.name}`}, { $set: reqData});
-      res.end(JSON.stringify(reqData))
-    }
-  }
-  catch (err) {
-    res.status(500)
+  } catch (err) {
+    res.status(500);
     res.end(err);
   }
-})
+});
 
 var server = app.listen(8081, function () {
   var host = server.address().address;
